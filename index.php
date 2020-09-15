83df22e65b16ddde637cfc04ce80d070f271bf8c
Ivan <matteraivam@gmail.com>
2020-09-15 15:48:56 +0200

    varie prove

--- index.php
+++ index.php
@@ -2,5 +2,9 @@
  $text = 'Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.';
  $badwords = $_GET["badwords"];
  $textReplace = str_replace($badwords,'***',$text,);
- echo   $textReplace  .  '<br>La lunghezza della stringa è: ' . strlen($textReplace) ;
+ echo   ucwords($textReplace)  .  '<br>La lunghezza della stringa è: ' . strlen($textReplace) . '<br>' ;
+ $esperimento = explode('.',$textReplace);
+ print_r ($esperimento);
+
+ echo   '<br>' .strpos($textReplace, 'elit'). ' posizione elit';
  ?>
